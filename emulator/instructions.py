0485ae13ac870e0eceea1139c10c9f67f18a0d18
kElnaDev <kelna@kelna.dev>
2024-03-27 23:52:34 +1100

    added inc then added .loop_test for some fun

--- emulator/instructions.py
+++ emulator/instructions.py
@@ -8,6 +8,7 @@ def run_instruction(memory_banks: list[emulator.memory.Memory], instruction: int
         6: ser,
         12: bsr,
         13: add,
+        17: inc,
         19: comp,
         20: jlt,
         21: jle,
@@ -50,6 +51,31 @@ def add(memory_banks: list[emulator.memory.Memory], instruction_address: int):
 
     arr1 = memory_banks[MEMBANK.REG.value].get_value(address_to_add_to, A_SIZE)
     arr2 = memory_banks[MEMBANK.REG.value].get_value(address_to_add_from, A_SIZE)
+    
+    result = []
+    carry = False
+
+    for bit1, bit2 in zip(reversed(arr1), reversed(arr2)):
+        # Perform binary addition considering the carry
+        sum_bits = bit1 + bit2 + carry
+
+        # Calculate the result bit and the carry for the next iteration
+        result_bit = sum_bits % 2
+        carry = sum_bits // 2
+
+        # Add the result bit to the beginning of the result list
+        result.insert(0, result_bit == 1)
+
+    FC = emulator.memory.mnemonic_to_adddress('FC')
+    memory_banks[MEMBANK.REG.value].set_value(FC[0], [carry])
+
+    memory_banks[MEMBANK.REG.value].set_value(address_to_add_to, result)
+
+def inc(memory_banks: list[emulator.memory.Memory], instruction_address: int):
+    address_to_add_to = get_arg_value(memory_banks[MEMBANK.ROM.value], instruction_address, 0)
+
+    arr1 = memory_banks[MEMBANK.REG.value].get_value(address_to_add_to, A_SIZE)
+    arr2 = emulator.memory.int_to_bit_array(1, A_SIZE)
     
     result = []
     carry = False
