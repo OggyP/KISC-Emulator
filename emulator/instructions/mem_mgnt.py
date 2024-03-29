ba8cee83750161d0d55603310c18a0bd54d9cd07
kElnaDev <kelna@kelna.dev>
2024-03-29 16:56:20 +1100

    added sem

--- emulator/instructions/mem_mgnt.py
+++ emulator/instructions/mem_mgnt.py
@@ -1,7 +1,7 @@
 from emulator.instructions.general_functions import get_arg_value
 import emulator.memory
 from compiler.instructions import I_SIZE, A_SIZE, MAX_INT
-from emulator.memory import MEMBANK
+from emulator.memory import MEMBANK, bit_array_to_int
 
 
 def ser(memory_banks: list[emulator.memory.Memory], instruction_address: int):
@@ -9,6 +9,18 @@ def ser(memory_banks: list[emulator.memory.Memory], instruction_address: int):
         memory_banks[MEMBANK.ROM.value], instruction_address, 0)
     memory_banks[MEMBANK.REG.value].set_value(
         address_to_load,
+        memory_banks[MEMBANK.ROM.value].get_value(
+            instruction_address + I_SIZE + A_SIZE, A_SIZE
+        ),
+    )
+
+
+def sem(memory_banks: list[emulator.memory.Memory], instruction_address: int):
+    register_address = get_arg_value(
+        memory_banks[MEMBANK.ROM.value], instruction_address, 0)
+    address_to_load = memory_banks[MEMBANK.REG.value].get_value(register_address, A_SIZE)
+    memory_banks[MEMBANK.RAM.value].set_value(
+        bit_array_to_int(address_to_load),
         memory_banks[MEMBANK.ROM.value].get_value(
             instruction_address + I_SIZE + A_SIZE, A_SIZE
         ),
